28c5dd4463219b613faebfe22527bb4e209ba304
misakadam97 <misakadam97@gmail.com>
2022-05-30 12:57:11 +0200

    impemented predict_nuclei function for CellPoseDetector class

--- cellanalysis/cellanalysis.py
+++ cellanalysis/cellanalysis.py
@@ -30,19 +30,19 @@ class Image:
             channel_names (dict): names of the channels, defaults to {1:'1',2:'2',3:'3'}
 
         """
-        
+
         self.folder = folder
         self.name = name
         self.channel_names = channel_names
-        
+
 
     def load_image(self):
         """loads the image data and stores it in self.image"""
         """loads the image data and stores it in self.image"""
 
         pass
-    
-    def display_image(self): 
+
+    def display_image(self):
         channels = self.image.shape[2]
         plt.figure(figsize=(5*channels,5))
         for channel in range(channels):
@@ -60,10 +60,10 @@ class Image:
 
 
 class ZeissCziImage(Image):
-    
+
     def __init__(self, folder, name, channel_names = {1:'1', 2:'2', 3:'3'}):
         super().__init__(folder, name, channel_names)
-        
+
 
     def load_image(self):
         path = f"{self.folder}/{self.name}"
@@ -71,7 +71,7 @@ class ZeissCziImage(Image):
         self.image = aics_image.get_image_data('YXZ')
 
 class ImageXpressImage(Image):
-    
+
     def __init__(self, folder, name, channel_names = {1:'1', 2:'2', 3:'3'}):
         super().__init__(folder, name, channel_names)
 
@@ -81,7 +81,7 @@ class ImageXpressImage(Image):
         imgs = [im for im in imgs if f"_thumb" not in im]
         imgs.sort()
         imgs = [io.imread(f"{self.folder}/{im}") for im in imgs]
-        
+
         imgs = [im.reshape(im.shape[0], im.shape[1], 1) for im in imgs]
         self.image = np.concatenate(imgs, axis=2)
 
@@ -89,7 +89,7 @@ class ImageXpressImage(Image):
 # +
 class CellDetector:
     """class with cell and nucleus detecting methods and detector specific attributes"""
-    
+
     def __init__(self):
         pass
 
@@ -102,27 +102,33 @@ class CellDetector:
         return blobs_labels
 
     def predict_nuclei(self, img, nucleus_channel=1):
-        nucleus_channel -=1 
+        nucleus_channel -=1
         global_thresh = threshold_otsu(img[:,:,nucleus_channel])
         binary_global = img[:,:,nucleus_channel] > global_thresh
         binary_closed = binary_closing(binary_global, np.ones(shape=(10,10)))
         blobs_labels = measure.label(binary_closed, background=0)
         return blobs_labels
-    
+
 class CellPoseDetector:
     """class with cell and nucleus detecting methods and detector specific attributes"""
-    
+
     def __init__(self):
         self.cell_model = models.Cellpose(gpu=False, model_type='cyto')
-        #self.nucleus_model = 
+        self.nucleus_model = models.Cellpose(gpu=False, model_type='nuclei')
 
     def predict_cells(self, img, channels=[3,1], diameter=300, **kwargs):
         """...."""
         masks, flows, styles, diams = self.cell_model.eval(img, channels=channels, diameter=diameter, **kwargs)
         return masks
 
-    def predict_nuclei(self, img, nucleus_channel=1):
-        pass
+    def predict_nuclei(self, img, nucleus_channel=1, diameter=300, **kwargs):
+        """...."""
+        masks, flows, styles, diams = \
+            self.nucleus_model.eval(img,
+                                    channels=[nucleus_channel, 0],
+                                    diameter=diameter,
+                                    **kwargs)
+        return masks
 
 
 # -
@@ -151,11 +157,11 @@ class Experiment:
 
 
 class Analyzer:
-    
+
     def __init__(self, detector):
-        
+
         self.detector = detector
-    
+
     def analyze(self, img, nucleus_channel):
         nuclei = self.detector.predict_nuclei(img, nucleus_channel)
         img = img[:,:,nucleus_channel].reshape(-1,1)
@@ -164,7 +170,7 @@ class Analyzer:
         data = np.concatenate((nuclei, img), axis = 1)
         data = pd.DataFrame(data, columns = ['cell_id', 'fluorescence'])
         data['size'] = 1
-        
+
         data = data.groupby('cell_id').sum()
         data['mean_fluorescence'] = data.fluorescence / data["size"]
 
